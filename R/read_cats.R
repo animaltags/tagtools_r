a7d06d4eaa2e02c1ebeb046acbc228c20465c239
Stacy DeRuiter <stacy.lynn.deruiter@gmail.com>
2025-11-19 16:25:49 -0500

    update read_cats to deal with multiple input files

--- R/read_cats.R
+++ R/read_cats.R
@@ -1,12 +1,14 @@
 
-#' Read a CATS data file and convert to .nc
+#' Read CATS csv data file(s) and convert to .nc
 #'
-#' Read a .csv file with data from a CATS tag deployment, including associated metadata, and store the resulting data in a .nc file.
-#' @param fname is the file name of the CATS CSV file including the complete
-#' path name if the file is not in the current working directory or in a
-#' directory on the path. The .csv suffix is optional.
-#' @param depid is a string containing the deployment identification code assigned
-#' to this deployment, for example, 'mn12_186a'.
+#' Read .csv file(s) with data from a CATS tag deployment, including associated metadata, and store the resulting data in a .nc file.
+#' @param file_dir String containing the name (including full or relative path) of the directory where the CATS csv file(s) are stored. If omitted, \code{fname} must include full or relative path information for the csv file(s).
+#' @param depid String containing the deployment identification code assigned
+#' to this deployment, for example, 'mn12_186a'. If \code{fname} is not input, csv files are assumed to have names of the form "\code{depid}_001.csv" (002, 003 etc if multiple files) or "\code{depid}.csv" (if data is in a single file).
+#' @param fname Name(s) of the CATS csv file(s) to read. If \code{fname} is not provided, then the function will try to read all csv files in \code{file_dir}. If \code{file_dir} is provided, the path(s) to the file(s) will be constructed by appending the file name(s) to the \code{file_dir}. 
+#' If \code{file_dir} is omitted, then \code{fname} is assumed to include the path to the file(s). The .csv file extension is optional.
+#' @param nc_dir String containing the name (including full or relative path) of the directory where the output nc file should be stored. Defaults to the current working directory.
+
 #' @return A string (constructed by: '\code{depid}_raw.nc'; for example, 'mn12_186a_raw.nc') containing the file name of the netCDF (.nc) file in which the output has been saved. This function
 #' generates a netCDF file in the current working directory containing
 #' 		the tag data variables, including:
@@ -19,24 +21,47 @@
 #' @note CATS loggers can produce very large csv files which are slow to
 #' process. This function is (somewhat) optimised for speed and memory use so will
 #' tolerate large files. But processing could be slow. Note also that although CATs tags use a NED axis orientation for 3D sensors, 
-#' this function converts to the NEU orientation expected by the animaltag tool kit. 
+#' **this function converts to the NEU orientation** expected by the animaltag tool kit. 
 #' To revert (if continuing analysis with CATs-specific tools outside animaltags), 
-#' simply multiply all z-axis values by -1. 
-#' Also note that Cade et al. 2021 note that not all CATs tags have the same internal orientation of the triaxial sensors -- 
+#' simply multiply all z-axis values by -1, and consider editing the metadata. 
+#' Also note that according to Cade et al. 2021, not all CATs tags have the same internal orientation of the triaxial sensors -- 
 #' such that the first column in the data may or may not be the "x axis." 
 #' Here, we assume that the three columns of data for any triaxial sensor are correctly labeled with X,Y,Z included in the column name in the CATs csv file. 
 #' If not, further data-based bench calibration of the device may be needed to determine correct axis orientation.
 #' @export
 #' @examples \dontrun{
-#' nc_filename <- read_cats("my_cats_file.csv", "my_cats_deplyment_name")
+#' nc_filename <- read_cats("my_cats_file.csv", "my_cats_deployment_name")
 #' load_nc("my_cats_deployment_name_raw.nc")
 #' }
-read_cats <- function(fname, depid) {
+read_cats <- function(file_dir = NULL, fname = NULL, depid, nc_dir = getwd()) {
+  if (is.null(fname) & !is.null(file_dir)){
+    # get file name(s) (with path included) 
+    # that are in file_dir
+    # and that contain depid and maybe a number at the end
+    # files will be sorted alphabetically so should be in order from 1....n (if multiple)
+    fname <- normalizePath(list.files(path = file_dir, 
+                                      pattern = paste0( depid, '.*\\d*\\.csv$'), 
+                                      full.names = TRUE))
+    if (length(fname) == 0){
+      # if there are not numbered files look for 
+      stop(paste0("No files found in folder: ", file_dir, "."))
+    }
+    # all info from file_dir is now in fname
+    file_dir <- NULL
+  }
+  
+  if (!is.null(file_dir)){
+    # get file name(s) with path if user input both file_dir and fname
+    fname <- file.path(file_dir, fname)
+  }
+  
+  
   V <- read_cats_csv(fname)
+  
   info <- list(
     depid = depid,
-    data_source = fname,
-    data_nfiles = "1",
+    data_source = paste0(fname, collapse = ", "),
+    data_nfiles = paste0(length(fname)),
     data_format = "csv",
     device_serial = NULL,
     device_make = "CATS",
@@ -81,16 +106,23 @@ read_cats <- function(fname, depid) {
   }
 
   info$sensors_list <- sl
-  nc_file <- paste(depid, "_raw.nc", sep = "")
+  nc_file <- file.path(nc_dir, 
+                       paste(depid, "_raw.nc", sep = ""))
 
+  # add sensor data to nc file
+  for (k in c(1:length(Sens))) {
+    if (SS[k]) {
+      cols <- grep(Sens[k], names(V))
+      save_sens_struct(V[, cols], depid, sampling_rate,
+        fname,
+        name = Sens[k], naxes = ax[k]
+      )
+    }
+  }
+  add_nc(nc_file, info, "info")
+  return(nc_file)
 
-  # ******************************
-  # CATs triaxial sensors are in NED orientation
-  # need to flip to -z for animaltags tools
-  # ******************************
-
-  #*********************************
-  # HELPER FUNCTION
+  # HELPER FUNCTION to save CATS sensor structure to nc file
   save_sens_struct <- function(X, depid, sampling_rate, fname, name, naxes) {
     nc_file <- paste(depid, "_raw.nc", sep = "")
     cols <- grep(name, names(X))
@@ -130,22 +162,9 @@ read_cats <- function(fname, depid) {
         S$unit_name <- "counts"
         S$unit_label <- "counts"
       }
-
+      
       add_nc(nc_file, S, name)
     }
   } # end of save_sens_struct
-  #********************************************************
-
-  # now add sensor data
-  for (k in c(1:length(Sens))) {
-    if (SS[k]) {
-      cols <- grep(Sens[k], names(V))
-      save_sens_struct(V[, cols], depid, sampling_rate,
-        fname,
-        name = Sens[k], naxes = ax[k]
-      )
-    }
-  }
-  add_nc(nc_file, info, "info")
-  return(nc_file)
 } # end of read_cats
+
--- R/read_cats_csv.R
+++ R/read_cats_csv.R
@@ -1,82 +1,100 @@
 #' Read a CSV file with sensor data from a CATS tag
 #'
-#' Read in data from a CATS tag deployment (stored in a .csv file). This function is usable by itself but is more normally
+#' Read in data from a CATS tag deployment (stored in one or more .csv files). This function is usable by itself but is more normally
 #' called by \code{\link[tagtools]{read_cats}} which handles metadata and creates a NetCDF file.
-#' @param fname is the file name of the CATS CSV file including the complete
-#' path name if the file is not in the current working directory or in a
-#' directory on the path. The .csv suffix is optional.
+#' @param fname is the file name of the CATS CSV file(s) including a full or relative path. The .csv suffix is optional.
 #' @param max_samps is optional and is used to limit reading to a maximum number of samples (rows) per sensor. This is useful to read in a part of a very large file
 #' for testing. If max_samps is not given, the entire file is read.
-#' @param skip_samps Number of lines of data to skip (excluding header) before starting to read in data. Defaults to 0 (start at the beginning of the file), but can be used to read in a part of a file, or to read in and process a large file in chunks.
+#' @param skip_samps Number of lines of data to skip (excluding header) before starting to read in data. Defaults to 0 (start at the beginning of the file), but could be used to read in a part of a file, or to read in and process a large file in chunks.
 #' @return A tibble data frame containing the data read from the file. The column names are
 #' taken from the first line of the CSV file and include units and axis. Some columns may be empty (if for example, a tag did not record data from a certain sensor type).
 #' @note CATS csv files can be extremely large; perhaps too large to read the entire file into memory at once and work with it.
 
 read_cats_csv <- function(fname, max_samps = Inf, skip_samps = 0) {
-  #****************************
-  # check inputs
-  #****************************
-  if (missing(fname)) {
-    stop("File name fname is required for read_cats_csv.\n")
-  }
-
-  if (missing(max_samps) | is.null(max_samps)) {
-    max_samps <- Inf
-  }
-
-  # append .csv if needed
-  if (stringr::str_sub(fname, -4) != ".csv") {
-    fname <- paste(fname, ".csv", sep = "")
-  }
-
-  #****************************
-  # end of input checks
-  #****************************
-
-  # read data file with header
-  # note: readr has a function read_csv_chunked() to read in files part at a time,
-  # but it's not meant to then read ALL of them in and then paste together -
-  # may be useful though if ever actually doing processing on chunks.
-  V <- suppressMessages(readr::read_csv(
-    file = fname, col_names = TRUE,
-    col_types = readr::cols(
-     `Time (UTC)` = readr::col_character(),
-      `GPS (raw) 1 [raw]` = readr::col_character(),
-      `GPS (raw) 2 [raw]` = readr::col_character(),
-     `GPS 1` = readr::col_character(),
-     `GPS 2` = readr::col_character()
-    ),
-    na = c(NA, "", " "),
-    trim_ws = TRUE,
-    skip = skip_samps,
-    n_max = max_samps
-  )) |>
-    as.data.frame()
-  # make mus and degree symbols and superscripts not show up as black diamonds with question marks inside
-  # that make R throw errors...
-  names(V) <- iconv(names(V), from = "UTF-8", to = "ASCII", sub = "")
-
-  # add date-time in POSIX format
-  di <- which(stringr::str_detect(names(V), "Date "))
-  if (length(di) > 1){
-    di <- which(stringr::str_detect(names(V), "Date ") & 
-                  stringr::str_detect(names(V), "UTC"))
-  }
-  
-  ti <- which(stringr::str_detect(names(V), "Time "))
-  if (length(ti) > 1){
-    ti <- which(stringr::str_detect(names(V), "Time ") & 
-                  stringr::str_detect(names(V), "UTC"))
-  }
-  
-  old_options <- options()
-  on.exit(options(old_options))
-  
-  options(digits.secs = 6)
-  V$Datetime <- as.POSIXct(paste(
-    V[, di],
-    V[, ti]),
-    format = "%d.%m.%Y %H:%M:%OS", tz = 'UTC'
+  if (length(fname) > 1){
+    # make sure file names are given in order
+    fname <- sort(fname)
+    # make sure there are no gaps in file numbers
+    fnums <- as.numeric(
+      sub(
+        x = regmatches(
+          x = basename(fname),
+          regexpr(pattern = '\\d+\\.csv*$', 
+                  text = basename(fname))
+        ),
+        pattern = '.csv$',
+        replacement = ''
+      )
     )
+    
+    if (any(diff(fnums) > 1)){
+      warning('There are gaps in numbered .csv files. Concatenating files: \n', fname)
+    }
+  }
+  
+  # append .csv if needed
+  need_suffix <- !grepl(fname, pattern = "\\.csv$")
+  if (any(need_suffix)) {
+    fname[need_suffix] <- paste(fname[need_suffix], ".csv", sep = "")
+  }
+
+  # read data file(s) and concatenate if multiple files
+  V <- #ifelse(length(fname == 1),
+      #        read1_cats_csv(fname, skip_samps, max_samps),
+              do.call(what = rbind, 
+                      lapply(X = fname, 
+                             FUN = read1_cats_csv, 
+                             skip_samps = skip_samps, 
+                             max_samps = max_samps))
+           #   )
+  
+  # helper function to read a single CATS csv file
+  read1_cats_csv <- function(file, skip_samps, max_samps){
+    V <- suppressMessages(
+      readr::read_csv(
+        file = file, 
+        col_names = TRUE,
+        col_types = readr::cols(
+          `Time (UTC)` = readr::col_character(),
+          # `GPS (raw) 1 [raw]` = readr::col_character(),
+          # `GPS (raw) 2 [raw]` = readr::col_character(),
+          # `GPS 1` = readr::col_character(),
+          # `GPS 2` = readr::col_character()
+        ),
+        na = c(NA, "", " "),
+        trim_ws = TRUE,
+        skip = skip_samps,
+        n_max = max_samps
+      )) |>
+      as.data.frame()
+    # make mus and degree symbols and superscripts not show up as black diamonds with question marks inside
+    # that make R throw errors...
+    names(V) <- iconv(names(V), from = "UTF-8", to = "ASCII", sub = "")
+    
+    # add date-time in POSIX format
+    di <- which(stringr::str_detect(names(V), "Date "))
+    if (length(di) > 1){
+      di <- which(stringr::str_detect(names(V), "Date ") & 
+                    stringr::str_detect(names(V), "UTC"))
+    }
+    
+    ti <- which(stringr::str_detect(names(V), "Time "))
+    if (length(ti) > 1){
+      ti <- which(stringr::str_detect(names(V), "Time ") & 
+                    stringr::str_detect(names(V), "UTC"))
+    }
+    
+    old_options <- options()
+    on.exit(options(old_options))
+    
+    options(digits.secs = 6)
+    V$Datetime <- as.POSIXct(paste(
+      V[, di],
+      V[, ti]),
+      format = "%d.%m.%Y %H:%M:%OS", tz = 'UTC'
+    )
+    return(V)
+  } # end of read1_cats_csv()
   return(V)
-}
+} # end of read_cats_csv()
+
